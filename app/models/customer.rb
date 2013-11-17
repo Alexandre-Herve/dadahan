26794ed48f9e5917494b5a714d7770b83b34a2f9
aurelien herve <mail@aurelien-herve.com>
2013-11-17 17:26:04 +0100

    en cours

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,20 @@
 class Customer < ActiveRecord::Base
   def type 
+    if is_private
+      "privé" 
+    elsif is_public
+      "public"
+    elsif is_individual
+      'particulier'
+    else
+      nil
+    end
   end
-  def set_type!
+
+  def set_type!(key)
+    #is_public     = false
+    #is_private    = false
+    #is_individual = false
+    #self.send(key) = true
   end
 end
